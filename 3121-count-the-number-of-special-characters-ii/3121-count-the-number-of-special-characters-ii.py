c19b05c260c3185eef51dff7d4ebce6c96ace0ff
Advait Warang <warangadvait.3000@gmail.com>
2026-05-27 17:25:18 +0530

    Time: 65 ms (94.12%), Space: 21.6 MB (28.82%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
@@ -1,10 +1,8 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        occ={}
-        for pos, letter in enumerate(word):
-            if letter not in occ:occ[letter]=[pos]
-            else:occ[letter].append(pos)
+        reverse=word[::-1]
+        n=len(word)
         count=0
         for i in list(set(word.lower())):
-            if i in occ and i.upper() in occ and occ[i][-1]<occ[i.upper()][0]:count+=1
+            if i in word and i.upper() in word and n-1-reverse.index(i)<word.index(i.upper()):count+=1
         return count

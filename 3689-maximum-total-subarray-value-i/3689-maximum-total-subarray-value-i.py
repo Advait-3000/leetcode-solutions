9ce226943551960aed5107486b070dbd91c40741
Advait Warang <warangadvait.3000@gmail.com>
2026-06-09 12:02:43 +0530

    Time: 45 ms (10.4%), Space: 26.7 MB (15.2%) - LeetHub

--- 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.py
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.py
@@ -2,9 +2,6 @@ class Solution:
     def maxTotalValue(self, nums: List[int], k: int) -> int:
         n=len(nums)
         if n==1:return 0
-        elif n==2:
-            nums.sort()
-            return (nums[-1]-nums[0])*k
         else:
             nums.sort()
             return (nums[-1]-nums[0])*k

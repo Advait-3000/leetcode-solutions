77a3761d1988c6b88d1c2d5792e0061e4158f990
Advait Warang <warangadvait.3000@gmail.com>
2026-08-04 20:51:30 +0530

    Time: 3 ms (55.45%), Space: 19.2 MB (71.09%) - LeetHub

--- 3765-complete-prime-number/3765-complete-prime-number.py
+++ 3765-complete-prime-number/3765-complete-prime-number.py
@@ -1,19 +1,14 @@
 class Solution:
     def completePrime(self, num: int) -> bool:
-        def isPrime(n):
+        def prime(n):
             if n <= 1:return False
             if n == 2:return True
             if n % 2 == 0:return False
             for i in range(3, int(n**0.5) + 1, 2):
                 if n % i == 0:return False
             return True
-        prefix=[num]
-        suffix=[num]
-        for i in range(1,len(str(num))):
-            suffix.append(num%(10**i))
-            prefix.append(num//(10**i))
-        for i in prefix:
-            if not isPrime(i):return False
-        for i in suffix:
-            if not isPrime(i):return False
+        s=str(num)
+        for i in range(1, len(s) + 1):
+            if not prime(int(s[:i])) or not prime(int(s[-i:])):
+                return False
         return True
